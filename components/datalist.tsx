7c43c7d033f432f1f95bae496b387bdfd2b2a86b
ewan <ewan@MacBook-Pro-ewan.local>
2024-06-27 00:14:33 +0300

    Initial point

---
+++ components/datalist.tsx
@@ -0,0 +1,11 @@
+import React, { JSXElementConstructor, useState} from "react";
+
+
+// затестил создать компонент
+
+// export const myComp: JSX.Element = <div tabIndex={0}> Element JSX HERE !!!</div>
+
+// export default - експорт одного экз который можно импортнуть с другим алиасом
+// простой экспорт уместен когда куча экспортов из одного модуля
+
+export const myJSXComp: JSX.Element = <div tabIndex={0}> Element JSX HERE !!!</div>
